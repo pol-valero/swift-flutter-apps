9be762f03336ce74e5ef847efedd255f1c0fbb68
USER1 <user1@MacBook-Pro-de-USER1.local>
2023-10-08 21:16:20 +0200

    Added AC3 with view

---
+++ AC3/AC3/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  AC3
+//
+//  Created by Pol Valero on 6/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // Numbers
+    @IBOutlet weak var num0: UIButton!
+    @IBOutlet weak var num1: UIButton!
+    @IBOutlet weak var num2: UIButton!
+    @IBOutlet weak var num3: UIButton!
+    @IBOutlet weak var num4: UIButton!
+    @IBOutlet weak var num5: UIButton!
+    @IBOutlet weak var num6: UIButton!
+    @IBOutlet weak var num7: UIButton!
+    @IBOutlet weak var num8: UIButton!
+    @IBOutlet weak var num9: UIButton!
+    
+    @IBOutlet weak var opAdd: UIButton!
+    @IBOutlet weak var opSub: UIButton!
+    @IBOutlet weak var opDiv: UIButton!
+    @IBOutlet weak var opMul: UIButton!
+    @IBOutlet weak var opRes: UIButton!
+    @IBOutlet weak var opPercent: UIButton!
+    @IBOutlet weak var opAc: UIButton!
+    @IBOutlet weak var opPlusMinus: UIButton!
+    @IBOutlet weak var opComma: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+}
+
