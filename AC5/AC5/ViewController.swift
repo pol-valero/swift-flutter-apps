3596ddc83134cc89bda2d4cadec6568d70288e6e
Pol Valero <pol.valero.arjona@gmail.com>
2023-11-16 11:57:46 +0100

    UI structure finished. When the API does not respond, the skeleton with some default info is displayed

--- AC5/AC5/ViewController.swift
+++ AC5/AC5/ViewController.swift
@@ -21,7 +21,8 @@ class ViewController: UIViewController {
     var weather: Weather = Weather()
     
     var weatherForecast: [Weather] = []
-
+    
+    //UI WEATHER LABELS
     @IBOutlet weak var cityLabel: UILabel!
     
     @IBOutlet weak var tempLabel: UILabel!
@@ -29,12 +30,61 @@ class ViewController: UIViewController {
     @IBOutlet weak var meteoLabel: UILabel!
     
     @IBOutlet weak var maxMinTempLabel: UILabel!
+    /*/              */
+    
+    //UI FORECAST LABELS
+    @IBOutlet weak var d1DateLabel: UILabel!
+    @IBOutlet weak var d1MeteoDescriptionLabel: UILabel!
+    @IBOutlet weak var d1TempLabel: UILabel!
+    
+    @IBOutlet weak var d2DateLabel: UILabel!
+    @IBOutlet weak var d2MeteoDescriptionLabel: UILabel!
+    @IBOutlet weak var d2TempLabel: UILabel!
+    
+    @IBOutlet weak var d3DateLabel: UILabel!
+    @IBOutlet weak var d3MeteoDescriptionLabel: UILabel!
+    @IBOutlet weak var d3TempLabel: UILabel!
+    
+    
+    @IBOutlet weak var d4DateLabel: UILabel!
+    @IBOutlet weak var d4MeteoDescriptionLabel: UILabel!
+    @IBOutlet weak var d4TempLabel: UILabel!
+
+    @IBOutlet weak var d5DateLabel: UILabel!
+    @IBOutlet weak var d5MeteoDescriptionLabel: UILabel!
+    @IBOutlet weak var d5TempLabel: UILabel!
+    /*/               */
+    
+    
+    
     
     func changeWeatherLabels() {
         cityLabel.text = "Barcelona"
         tempLabel.text = weather.temp! + "ºC"
         meteoLabel.text = weather.meteo! + " - " + weather.meteoDescription!
         maxMinTempLabel.text = "Max. " + weather.maxTemp! + "ºC  " + "Min. " + weather.minTemp! + "ºC "
+    }
+    
+    func changeWeatherForecastLabels() {
+        d1DateLabel.text = weatherForecast[0].date
+        d1MeteoDescriptionLabel.text = weatherForecast[0].meteo
+        d1TempLabel.text = weatherForecast[0].temp! + "ºC"
+        
+        d2DateLabel.text = weatherForecast[1].date
+        d2MeteoDescriptionLabel.text = weatherForecast[1].meteo
+        d2TempLabel.text = weatherForecast[1].temp! + "ºC"
+        
+        d3DateLabel.text = weatherForecast[2].date
+        d3MeteoDescriptionLabel.text = weatherForecast[2].meteo
+        d3TempLabel.text = weatherForecast[2].temp! + "ºC"
+        
+        d4DateLabel.text = weatherForecast[3].date
+        d4MeteoDescriptionLabel.text = weatherForecast[3].meteo
+        d4TempLabel.text = weatherForecast[3].temp! + "ºC"
+        
+        d5DateLabel.text = weatherForecast[4].date
+        d5MeteoDescriptionLabel.text = weatherForecast[4].meteo
+        d5TempLabel.text = weatherForecast[4].temp! + "ºC"
     }
     
     override func viewDidLoad() {
@@ -49,25 +99,35 @@ class ViewController: UIViewController {
                 self.weather.minTemp = response.tempMin
                 self.weather.maxTemp = response.tempMax
                 
-                self.changeWeatherLabels()
+                //Only if we receive a valid response, we display the information
+                if (self.weather.meteo != nil) {
+                    self.changeWeatherLabels()
+                }
             }
         }
         
 
        
 
-        APIManager.shared.requestForecastForCity("Barcelona", "es") { (response) in
-            
-            for item in response.list {
-                print("----------------")
+        APIManager.shared.requestForecastForCity("Barcelona", "es") {
+            (response) in DispatchQueue.main.async {
                 
-                self.weatherForecast.append(Weather(date: item.date, meteo: item.main, meteoDescription: item.description, temp: item.temp))
                 
-                print(self.weatherForecast.last)
+                for item in response.list {
+                    
+                    self.weatherForecast.append(Weather(date: item.date, meteo: item.main, meteoDescription: item.description, temp: item.temp))
+                    
+                    //print(self.weatherForecast.last)
+                }
+                
+                //Only if we have received the 5 day forecast, we display the information
+                if (response.list.count == 5) {
+                    self.changeWeatherForecastLabels();
+                }
+                
             }
         }
 
-        // print the structure saved
 
     }
 
