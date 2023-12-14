2bb7f7acc3ddf09fd3162de4b3195b8ef6d54c8a
Pol Valero <pol.valero.arjona@gmail.com>
2023-12-14 20:29:32 +0100

    Created recipe list view

--- AC7/lib/recipes.dart
+++ AC7/lib/recipes.dart
@@ -1,16 +1,123 @@
 import 'package:flutter/material.dart';
 
-//TODO: Do the class properly is only for testing purposes (redirecting to recipes view)
-class RecipesView extends StatelessWidget {
+class RecipesView extends StatefulWidget {
+  const RecipesView({Key? key}) : super(key: key);
+
+  @override
+  State<RecipesView> createState() => _RecipesViewState();
+}
+
+class _RecipesViewState extends State<RecipesView>  {
+
+  var recipes = [
+    const Recipe(
+      name: 'Recipe 1',
+      description: 'Description',
+    ),
+    const Recipe(
+      name: 'Recipe 2',
+      description: 'Description',
+    ),
+    const Recipe(
+      name: 'Recipe 3',
+      description: 'Description',
+    ),
+    const Recipe(
+      name: 'Recipe 4',
+      description: 'Description',
+    ),
+    const Recipe(
+      name: 'Recipe 5',
+      description: 'Description',
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Recipes'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const RecipesView()),
+              );
+            },
+            icon: const Icon(Icons.add),
+          ),
+        ],
       ),
-      body: Center(
-        child: Text('Recipes View'),
+      body: SafeArea(
+        child: ListView.builder(
+          itemCount: recipes.length,
+          itemBuilder: (context, index) {
+            return InkWell(
+              child: Container(
+                height: 100,
+                margin: const EdgeInsets.all(15),
+                padding: const EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(13),
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            recipes[index].name,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            recipes[index].description,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(width: 15),
+                    Container(
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Image.asset('assets/recipe_img.png'),
+                    ),
+                  ],
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const RecipesView()),
+                );
+              },
+            );
+          }
+        ),
       ),
     );
   }
+}
+
+class Recipe {
+
+  final String name;
+  final String description;
+
+  const Recipe({required this.name, required this.description});
 }
